3ef78602d625c8539995c2da4aa91f4ac8b3486d
denosawr <denosawr@vivaldi.net>
2019-02-25 19:18:27 +0800

    Better network detection
    Makes awk sum columns instead of just using the first interface (which is sometimes not the main one)

--- scripts/network.sh
+++ scripts/network.sh
@@ -5,7 +5,8 @@ exists () {
 }
 
 if exists /usr/local/bin/ifstat ; then
-	echo "↓ $(/usr/local/bin/ifstat -n -z -S 1 1 | awk 'FNR == 3 {print $2}') ↑ $(/usr/local/bin/ifstat -n -z -S 1 1 | awk 'FNR == 3 {print $3}')"
+    NETWORKUSAGE=$(/usr/local/bin/ifstat -n -z -S 2 1)
+	echo "↓ $(echo "$NETWORKUSAGE" | awk 'FNR == 3 {for(x=1;x<=NF;++x)if(x % 2 == 0)sum+=$x} END {print sum}') ↑ $(echo "$NETWORKUSAGE" | awk 'FNR == 3 {for(x=1;x<=NF;++x)if(x % 2 == 1)sum+=$x} END {print sum}')"
 else
     echo "Please install ifstat: `brew install ifstat`."
 fi
