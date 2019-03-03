e847e317d22452e25d56baa2e02618be718e48e8
denosawr <denosawr@vivaldi.net>
2019-03-03 18:31:40 +0800

    Primitive menu bar app [WIP]

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const fs = require("fs");
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require("electron");
 const utils = require(__dirname + "/app/js/utils.js");
-const cp = require("child_process");
+const opn = require("opn");
+const path = require("path");
 
-let lastProcessCount = 0;
-let processes = {};
+const homedir = require("os").homedir();
 
 process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = true; // disables CSP/unsafe-eval warning
 let arguments_ = Array.from(process.argv);
@@ -48,6 +47,49 @@ function createWindow() {
     // and load the index.html of the app.
     win.loadFile(__dirname + "/app/index.html");
     win.setSize(display.size.width + 12, menuBarHeight + 2);
+
+    tray = new Tray("app/assets/iconTrayTemplate.png");
+    const contextMenu = Menu.buildFromTemplate([
+        { id: "desktopName", label: "Current wallpaper name:", enabled: false },
+
+        { type: "separator" },
+
+        {
+            label: "Show Configuration File",
+            click: () => {
+                opn(path.join(homedir, ".config/tourmaline.json"));
+            },
+        },
+        {
+            label: "Open Plugins Folder",
+            click: () => {
+                opn(homedir);
+            },
+        },
+
+        { type: "separator" },
+
+        {
+            label: "About Tourmaline",
+            click: () => {
+                opn("https://github.com/denosawr/tourmaline");
+            },
+        },
+        {
+            label: "Open Developer Tools",
+            click: () => {
+                win.openDevTools({ mode: "detach" });
+            },
+        },
+        {
+            label: "Quit Tourmaline",
+            click: () => {
+                app.quit();
+            },
+        },
+    ]);
+    tray.setToolTip("This is my application.");
+    tray.setContextMenu(contextMenu);
 
     return win;
 }
