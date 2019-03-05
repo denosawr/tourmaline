916b3a38d952f9029be7bb7eb85f95f5539fe7f4
denosawr <denosawr@vivaldi.net>
2019-03-05 19:09:07 +0800

    Fixed "Microsoft <app>" bug

--- tourmaline-helper/Sources/tourmaline-helper/main.swift
+++ tourmaline-helper/Sources/tourmaline-helper/main.swift
@@ -221,12 +221,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         socket.emit("window-change", ["Apple", applicationName])
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {  // this delay may need to be changed
-            var menuBarItems = getMenuItemTitles()
+            let menuBarItems = getMenuItemTitles()
             if !menuBarItems.isEmpty {
-                menuBarItems[1] = applicationName
                 socket.emit("window-change", menuBarItems)
+            } else {
+                socket.emit("window-change", ["Apple", applicationName])
             }
         }
+        //}
     }
     
     
